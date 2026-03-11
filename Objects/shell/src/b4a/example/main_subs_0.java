b0928e5ea3f85942dea491066b1220d20a668ef9
kohiiekei <kaikakobayashi@gmail.com>
2026-03-11 15:34:40 +0800

    b4a

---
+++ Objects/shell/src/b4a/example/main_subs_0.java
@@ -0,0 +1,87 @@
+package b4a.example;
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.pc.*;
+
+public class main_subs_0 {
+
+
+public static RemoteObject  _activity_create(RemoteObject _firsttime) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Create (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,24);
+if (RapidSub.canDelegate("activity_create")) { return b4a.example.main.remoteMe.runUserSub(false, "main","activity_create", _firsttime);}
+Debug.locals.put("FirstTime", _firsttime);
+ BA.debugLineNum = 24;BA.debugLine="Sub Activity_Create(FirstTime As Boolean)";
+Debug.ShouldStop(8388608);
+ BA.debugLineNum = 25;BA.debugLine="Activity.LoadLayout(\"Layout\")";
+Debug.ShouldStop(16777216);
+main.mostCurrent._activity.runMethodAndSync(false,"LoadLayout",(Object)(RemoteObject.createImmutable("Layout")),main.mostCurrent.activityBA);
+ BA.debugLineNum = 26;BA.debugLine="End Sub";
+Debug.ShouldStop(33554432);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_pause(RemoteObject _userclosed) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Pause (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,32);
+if (RapidSub.canDelegate("activity_pause")) { return b4a.example.main.remoteMe.runUserSub(false, "main","activity_pause", _userclosed);}
+Debug.locals.put("UserClosed", _userclosed);
+ BA.debugLineNum = 32;BA.debugLine="Sub Activity_Pause (UserClosed As Boolean)";
+Debug.ShouldStop(-2147483648);
+ BA.debugLineNum = 34;BA.debugLine="End Sub";
+Debug.ShouldStop(2);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_resume() throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Resume (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,28);
+if (RapidSub.canDelegate("activity_resume")) { return b4a.example.main.remoteMe.runUserSub(false, "main","activity_resume");}
+ BA.debugLineNum = 28;BA.debugLine="Sub Activity_Resume";
+Debug.ShouldStop(134217728);
+ BA.debugLineNum = 30;BA.debugLine="End Sub";
+Debug.ShouldStop(536870912);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _globals() throws Exception{
+ //BA.debugLineNum = 20;BA.debugLine="Sub Globals";
+ //BA.debugLineNum = 22;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+
+public static void initializeProcessGlobals() {
+    
+    if (main.processGlobalsRun == false) {
+	    main.processGlobalsRun = true;
+		try {
+		        main_subs_0._process_globals();
+starter_subs_0._process_globals();
+main.myClass = BA.getDeviceClass ("b4a.example.main");
+starter.myClass = BA.getDeviceClass ("b4a.example.starter");
+		
+        } catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
+}public static RemoteObject  _process_globals() throws Exception{
+ //BA.debugLineNum = 15;BA.debugLine="Sub Process_Globals";
+ //BA.debugLineNum = 18;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+}
